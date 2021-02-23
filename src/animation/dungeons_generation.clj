b2cfa505f4fd29e6831d0052c50548b159f7eadd
Clement Van Truyart <me@trosa.io>
2021-02-23 23:31:29 +0100

    Dungeongen wip

---
+++ src/animation/dungeons_generation.clj
@@ -0,0 +1,15 @@
+(ns animation.dungeons-generation
+  "Generate dungeons")
+
+(comment
+  "Datastructure Goal:
+   - A tree with junction distance as metadata
+   - As Left leaf largest sized node / right rest
+    (Maybe use something like binnary tree ?)
+   - Recursively apply depth according parameter")
+
+(defn generate-basemap [width height]
+  {})
+
+(defn generate-dungeon [width height depth-thresold]
+  "")
